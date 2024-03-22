060db946b12c8338543661aa89def0be6c631c8f
HeesunJung <52689880+glee623@users.noreply.github.com>
2024-03-22 11:50:56 +0900

    Title: 살 수 있는 물건, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240322/살 수 있는 물건/things-able-to-buy.py
+++ 240322/살 수 있는 물건/things-able-to-buy.py
@@ -2,7 +2,9 @@ lists = {'book':3000, 'mask':1000}
 lists = sorted(lists.items(), key = lambda d:d[1], reverse = True )
 
 n = int(input())
+out = 'no'
 for thing,price in lists:
     if price <= n:
-        print(thing)
+        out = thing
         break
+print(out)
