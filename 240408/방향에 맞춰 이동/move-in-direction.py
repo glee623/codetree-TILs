3eb4d15c3fa6048b2da664f91eb8c7c9ef2126fa
HeesunJung <52689880+glee623@users.noreply.github.com>
2024-04-08 22:28:14 +0900

    Title: 방향에 맞춰 이동, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240408/방향에 맞춰 이동/move-in-direction.py
+++ 240408/방향에 맞춰 이동/move-in-direction.py
@@ -7,19 +7,15 @@ for n in range(N):
     step = int(step)
     if way == 'W':
         nx, ny = x + dx[2]*step, y + dy[2]*step
-        if 0<=nx and 0<=ny:
-            x, y = nx, ny
+        x, y = nx, ny
     elif way == 'S':
         nx, ny = x + dx[1]*step, y + dy[1]*step
-        if 0<=nx and 0<=ny:
-            x, y = nx, ny
+        x, y = nx, ny
     elif way == 'N':
         nx, ny = x + dx[3]*step, y + dy[3]*step
-        if 0<=nx and 0<=ny:
-            x, y = nx, ny
+        x, y = nx, ny
     elif way == 'E':
         nx, ny = x + dx[0]*step, y + dy[0]*step
-        if 0<=nx and 0<=ny:
-            x, y = nx, ny
+        x, y = nx, ny
 
 print(x, y)
