ea828ea6816299f06d13195e62d8cd4a877c1ab0
HeesunJung <52689880+glee623@users.noreply.github.com>
2024-04-18 00:41:59 +0900

    Title: 모이자, Time: 116ms, Memory: 25MB, Status: Passed - CodeTree

--- 240418/모이자/gather.py
+++ 240418/모이자/gather.py
@@ -1,15 +1,12 @@
+import sys
+
 n = int(input())
-arrs = list(map(int, input().split()))
+arrs = list(map(int,input().split()))
 
-max_res = -9999
+min_res = sys.maxsize
 for i in range(n):
-    arrs[i] *= 2
     diff = 0
-    for j in range(n-1):
-        diff += abs(arrs[j+1]-arrs[j])
-
-    if max_res < diff:
-        max_res = diff
-    arrs[i] //= 2
-
-print(max_res)
+    for j in range(n):
+        diff += (arrs[j]*abs(i-j))
+    min_res = min(min_res, diff)
+print(min_res)
